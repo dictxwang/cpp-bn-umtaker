0d66ecba258b4287287f43d20dc7d55dd217e90d
dictwang <dictwang@163.com>
2025-06-05 22:28:06 +0800

    change update script

--- bin/update.sh
+++ bin/update.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+PROJECT_ROOT=$(cd `dirname $0`; cd ..; pwd;)
+cd ${PROJECT_ROOT}
+
+cd 3rdparty/binancecpp/
+git checkout main
+git pull
+
+cd -
 git pull
 git submodule update --init --recursive
