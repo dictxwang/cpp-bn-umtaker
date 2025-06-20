ab2d8535a5045620a46d4cac8b8a55f6c9a56b39
dictwang <dictwang@163.com>
2025-06-20 21:55:51 +0800

    fix bug

--- src/trader/global_context.cpp
+++ src/trader/global_context.cpp
@@ -48,6 +48,7 @@ namespace trader {
     }
 
     void GlobalContext::init_best_path_order_limiter(TraderConfig& config) {
+        this->order_best_path_limiter = make_shared<AutoResetOrderLimiterBoss>();
         this->order_best_path_limiter->init(64, 1, config.order_account_limit_per_10seconds, 10, config.order_account_limit_per_minute, 1);
         this->order_best_path_limiter->start();
 
